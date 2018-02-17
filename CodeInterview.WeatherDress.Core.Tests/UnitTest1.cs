3cf823b359cea51ea06a12263a283108545462ed
srikanth.nettem <srikanth.nettem@gmail.com>
2018-02-16 20:39:11 -0800

    Initial Comit

---
+++ CodeInterview.WeatherDress.Core.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+namespace CodeInterview.WeatherDress.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
