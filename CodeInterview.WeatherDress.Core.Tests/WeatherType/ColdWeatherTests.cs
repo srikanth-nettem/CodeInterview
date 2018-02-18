b20d0a9ec6edc797bdc42a3af3c80ad3123d8805
srikanth.nettem <srikanth.nettem@gmail.com>
2018-02-17 16:15:16 -0800

    unit tests for weather

---
+++ CodeInterview.WeatherDress.Core.Tests/WeatherType/ColdWeatherTests.cs
@@ -0,0 +1,74 @@
+﻿using CodeInterview.WeatherDress.Core.WeatherType;
+using Xunit;
+using NSubstitute;
+
+namespace CodeInterview.WeatherDress.Core.Tests.WeatherType
+{
+    public class ColdWeatherTests
+    {
+        private IWeatherType _coldWeather;
+        private IWriter _writerMock;
+
+        public ColdWeatherTests()
+        {
+            _writerMock = Substitute.For<IWriter>();
+            _coldWeather = new ColdWeather(_writerMock);
+        }
+
+        [Fact(DisplayName = "Should take off pajamas on Pajamas_Off Command")]
+        public void ShouldTakeOffPajamas()
+        {
+            _coldWeather.TakeOffPajamas();
+            _writerMock.Received().Write("Removing PJs");
+        }
+
+        [Fact(DisplayName = "Should leave house on LeaveHouse Command")]
+        public void ShouldLeaveHouse()
+        {
+            _coldWeather.LeaveHouse();
+            _writerMock.Received().Write("leaving house");
+        }
+
+        [Fact(DisplayName = "Should Dress pants on Pants Command in Cold Weather.")]
+        public void ShouldWearShortsWhenAskedToPutOnPants()
+        {
+            _coldWeather.PutOnPants();
+            _writerMock.Received().Write("pants");
+        }
+
+        [Fact(DisplayName = "Should wear jacket on Jacket Command in Cold Weather")]
+        public void ShouldFailOnJacketCommand()
+        {
+            _coldWeather.PutOnJacket();
+            _writerMock.Received().Write("jacket");
+        }
+
+        [Fact(DisplayName = "Should Wear socks on Socks Command in Cold Weather")]
+        public void ShouldFailOnSocksCommand()
+        {
+            _coldWeather.PutOnSocks();
+            _writerMock.Received().Write("socks");
+        }
+
+        [Fact(DisplayName = "Should Dress t-shirt For Shirt Command in Cold Weather.")]
+        public void ShouldWearShirtWhenAskedToPutOnShirt()
+        {
+            _coldWeather.PutOnShirt();
+            _writerMock.Received().Write("t-shirt");
+        }
+
+        [Fact(DisplayName = "Should wear hat For Headwear Command in Cold Weather.")]
+        public void ShouldWearsunvisorWhenAskedToPutOnHeadWear()
+        {
+            _coldWeather.PutOnHeadwear();
+            _writerMock.Received().Write("hat");
+        }
+
+        [Fact(DisplayName = "Should wear boots For Footwear Command in Cold Weather.")]
+        public void ShouldWearSandalsWhenAskedToPutOnFootwear()
+        {
+            _coldWeather.PutOnFootwear();
+            _writerMock.Received().Write("boots");
+        }
+    }
+}
---
+++ CodeInterview.WeatherDress.Core.Tests/WeatherType/HotWeatherTests.cs
@@ -0,0 +1,74 @@
+﻿using CodeInterview.WeatherDress.Core.WeatherType;
+using Xunit;
+using NSubstitute;
+
+namespace CodeInterview.WeatherDress.Core.Tests.WeatherType
+{
+    public class HotWeatherTests
+    {
+        private IWeatherType _hotWeather;
+        private IWriter _writerMock;
+
+        public HotWeatherTests()
+        {
+            _writerMock = Substitute.For<IWriter>(); 
+            _hotWeather = new HotWeather(_writerMock);
+        }
+
+        [Fact(DisplayName = "Should Dress Shorts For Pants Command in Hot Weather.")]
+        public void ShouldWearShortsWhenAskedToPutOnPants()
+        {
+            _hotWeather.PutOnPants();
+            _writerMock.Received().Write("shorts");
+        }
+
+        [Fact(DisplayName = "Should fail when asked to wear Jacket in Hot Weather")]
+        public void ShouldFailOnJacketCommand()
+        {
+            _hotWeather.PutOnJacket();
+            _writerMock.Received().Write("fail");
+        }
+
+        [Fact(DisplayName = "Should fail when asked to wear socks in Hot Weather")]
+        public void ShouldFailOnSocksCommand()
+        {
+            _hotWeather.PutOnSocks();
+            _writerMock.Received().Write("fail");
+        }
+
+        [Fact(DisplayName = "Should Dress shirt For Shirt Command in Hot Weather.")]
+        public void ShouldWearShirtWhenAskedToPutOnShirt()
+        {
+            _hotWeather.PutOnShirt();
+            _writerMock.Received().Write("shirt");
+        }
+
+        [Fact(DisplayName = "Should wear sun-visor For Headwear Command in Hot Weather.")]
+        public void ShouldWearsunvisorWhenAskedToPutOnHeadWear()
+        {
+            _hotWeather.PutOnHeadwear();
+            _writerMock.Received().Write("sun visor");
+        }
+
+        [Fact(DisplayName = "Should wear sandals For Footwear Command in Hot Weather.")]
+        public void ShouldWearSandalsWhenAskedToPutOnFootwear()
+        {
+            _hotWeather.PutOnFootwear();
+            _writerMock.Received().Write("sandals");
+        }
+
+        [Fact(DisplayName = "Should take off pajamas on Pajamas_Off Command")]
+        public void ShouldTakeOffPajamas()
+        {
+            _hotWeather.TakeOffPajamas();
+            _writerMock.Received().Write("Removing PJs");
+        }
+
+        [Fact(DisplayName = "Should leave house on LeaveHouse Command")]
+        public void ShouldLeaveHouse()
+        {
+            _hotWeather.LeaveHouse();
+            _writerMock.Received().Write("leaving house");
+        }
+    }
+}
